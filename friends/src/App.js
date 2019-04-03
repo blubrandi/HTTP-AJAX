130d1e906bd2255f39bc1a9f20c66c2429056c6f
blubrandi <blubrandi0@gmail.com>
2019-04-02 21:03:59 -0600

    Created FriendList component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import FriendList from './Components/FriendList'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Friends App</h1>
+      <h1>App</h1>
+       <FriendList />
       </div>
     );
   }
